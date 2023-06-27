06f334f2398f422b0a4397990c16e213c3dacac9
Mireia Olivé <mirisfu@gmail.com>
2023-06-27 15:14:34 +0200

    login, main, register

---
+++ src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { MainComponent } from './components/main/main.component';
+
+import { ReactiveFormsModule } from '@angular/forms';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    MainComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+    //inicializacion conexión con firebase
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+//datos de conexión con firebase
+export const environment = {
+  firebase: {
+    projectId: 'sym-firebase-auth',
+    appId: '1:705269539204:web:afeefa9fa612450320fdd1',
+    storageBucket: 'sym-firebase-auth.appspot.com',
+    apiKey: 'AIzaSyDUR6xWo8Em1OHWAzZPNq7r-U63t-iucdo',
+    authDomain: 'sym-firebase-auth.firebaseapp.com',
+    messagingSenderId: '705269539204',
+  },
+  production: false,
+};
